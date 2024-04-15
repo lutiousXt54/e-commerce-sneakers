30d14fa791096defc38f78cf836779dd9de4bdcf
lutiousXt54 <abhradeepdebnath@gmail.com>
2024-04-15 12:36:33 +0530

    adding slide bar

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const menuFunc =()=>{
+    const menu = document.createElement('div');
+    const option = document.querySelector('.option');
+    menu.className='menu-bar';
+    const categories = document.querySelector('.categories');
+    const socialMedia = document.querySelector('.social');
+    const heading=document.createElement('div');
+    heading.className='head';
+    const h2Node = document.createElement('h2');
+    h2Node.textContent='menu';
+    const imgCancel = document.createElement('img');
+    imgCancel.className='remove';
+    imgCancel.src='./assets/images/cross.svg';
+    heading.appendChild(h2Node);
+    heading.appendChild(imgCancel);
+    menu.appendChild(heading);
+    menu.appendChild(categories);
+    if(screen.width < 490){
+        menu.insertBefore(option,menu.querySelector('.social'));
+    }
+    menu.appendChild(socialMedia);
+    document.body.appendChild(menu)
+    const width = window.innerHeight;
+    menu.style.height=`${width}`;
+    imgCancel.addEventListener('click',()=>{
+        menu.style.display='none';
+    });
+    console.log(menu,heading,width);
+}
